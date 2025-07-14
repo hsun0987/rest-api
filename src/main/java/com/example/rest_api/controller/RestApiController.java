1aef44305b3ac48eb4bc5407a86617de7486cece
hsun0987 <hsun0987@daum.net>
2025-07-14 12:45:13 +0900

    [#1] REST API - GET 02
    1. Path variable : 주소 내에 정보 전달하는 방법
    http://localhost:8080/api/echo/{message}
    
    @PathVariable String message
    @PathVariable(name = "message") String msg
    
    2. 한글 깨지는 문제 해결
    file >  settings > File Encordings > 모두 UTF-8 로 변경 > 시스템 재시작

--- src/main/java/com/example/rest_api/controller/RestApiController.java
+++ src/main/java/com/example/rest_api/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.rest_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,21 @@ public class RestApiController {
     public String hello(){
         var html = "<html> <body> <h1> Hello Spring Boot </h1> </body> </html>";
         return html;
+    }
+
+    @GetMapping(path = "/echo/{message}/age/{age}/is-man/{isMan}")
+    public String echo(
+            @PathVariable(name = "message") String msg,
+            @PathVariable int age,
+            @PathVariable boolean isMan
+    ){
+        System.out.println("echo message : " + msg);
+        System.out.println("echo message : " + age);
+        System.out.println("echo message : " + isMan);
+
+        // 대문자로 변환
+        msg = msg.toUpperCase();
+
+        return msg;
     }
 }
