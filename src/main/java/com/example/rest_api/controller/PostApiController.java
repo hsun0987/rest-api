cb38f7003b5c0d6c29bbadfed92019f0a0e9b0ed
hsun0987 <hsun0987@daum.net>
2025-07-15 19:45:24 +0900

    [#3] REST API - POST 01
    Headers + Body
    헤더에는  Content-type(text/json/xml/html) 을 포함
    
    @RequestBody -> 객체로 값을 받음
    json body 생성 {key : value}
    - String, Number(소수점, int, double, float), Boolean, {} : Object, [] : arrary
    
    chrome 스토어에서 api tester 설치 후 테스트!!

---
+++ src/main/java/com/example/rest_api/controller/PostApiController.java
@@ -0,0 +1,21 @@
+package com.example.rest_api.controller;
+
+
+import com.example.rest_api.model.BookRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public String post(
+            @RequestBody BookRequest bookRequest
+    ){
+        System.out.println(bookRequest);
+        return bookRequest.toString();
+    }
+}
