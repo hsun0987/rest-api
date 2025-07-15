064c3009f38818649690d28e57498c8e44cb94fe
hsun0987 <hsun0987@daum.net>
2025-07-15 20:27:28 +0900

    [#6] REST API - DELETE
    GetMappring 이랑 모양이 비슷하다
    path = { ... , ...} => 콤마를 이용하여 여러개의 주소를 지정할 수 있다

--- src/main/java/com/example/rest_api/controller/RestApiController.java
+++ src/main/java/com/example/rest_api/controller/RestApiController.java
@@ -1,8 +1,10 @@
 package com.example.rest_api.controller;
 
 import com.example.rest_api.model.BookQueryParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
@@ -44,5 +46,16 @@ public class RestApiController {
     ){
         System.out.println(a+b);
         System.out.println(a*b);
+    }
+
+
+    @DeleteMapping(path = {
+            "/user/{userName}/delete",
+            "/user/{userName}/del"
+    })
+    public void delete(
+            @PathVariable String userName
+    ){
+        log.info("user-name : {}", userName);
     }
 }
