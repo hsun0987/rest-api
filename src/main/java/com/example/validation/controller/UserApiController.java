19249fc7d783ede5dc21f0485f45c60faf4fd88e
hsun0987 <hsun0987@daum.net>
2025-07-18 14:49:39 +0900

    [#10] Spring Boot Validation 02
    클라이언트에서도 오류 사항 표시 O
    하지만 상태코드가 올바르지 않은 문제 해결
    
    exception handler 를 따로 만들기
     - 역할 분리로 비즈니스 로직에 더 집중할 수 있음
     - 값에 대해 유효하다는 보장을 할 수 있음
     - valid 를 통해 검증을 따로 직접 적어주지 않아도 됨

--- src/main/java/com/example/validation/controller/UserApiController.java
+++ src/main/java/com/example/validation/controller/UserApiController.java
@@ -5,6 +5,7 @@ import com.example.validation.model.UserRegisterRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,37 +20,12 @@ import java.util.stream.Collectors;
 public class UserApiController {
 
     @PostMapping("")
-    public Api<? extends Object> register(
+    public Api<UserRegisterRequest> register(
             @Valid
             @RequestBody
-            Api<UserRegisterRequest> userRegisterRequest,
-            BindingResult bindingResult
+            Api<UserRegisterRequest> userRegisterRequest
     ){
         log.info("init : {}", userRegisterRequest);
-
-        if(bindingResult.hasErrors()){
-            var errorMessageList = bindingResult.getFieldErrors().stream()
-                    .map(it -> {
-                        var format = "%s : { %s } 은 %s";
-                        var message = String.format(format, it.getField(), it.getRejectedValue(), it.getDefaultMessage());
-
-                        return message;
-                    }).collect(Collectors.toList());
-
-            var error = Api.Error
-                    .builder()
-                    .errorMessage(errorMessageList)
-                    .build();
-
-            var errorResponse = Api
-                    .builder()
-                    .resultCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-                    .resultMessage(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                    .error(error)
-                    .build();
-
-            return errorResponse;
-        }
 
         var body = userRegisterRequest.getData();
         Api<UserRegisterRequest> response = Api.<UserRegisterRequest>builder()
