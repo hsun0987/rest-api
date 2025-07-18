602d792b75fe6f93e05f2c53244da8a4fea91d5f
hsun0987 <hsun0987@daum.net>
2025-07-18 11:11:35 +0900

    [#9] Spring Boot 예외처리 실전 적용1
    1. 가데이터 삽입
    2. 없는 데이터 조회 시도 -> 예외 발생에 대한 처리
     : noSuchElement - 글로벌 핸들러가 아닌 특정 핸들러로 실행하도록 예외처리 생성

--- src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -3,10 +3,14 @@ package com.example.exception.exception;
 
 import com.example.exception.controller.RestApiBController;
 import com.example.exception.controller.RestApiController;
+import com.example.exception.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 // @RestControllerAdvice(basePackages = "com.example.exception.controller")
@@ -28,5 +32,21 @@ public class RestApiExceptionHandler {
     ){
         log.error("IndexOutOfBoundsExceptionHandler", e);
         return ResponseEntity.status(200).build();
+    }
+
+    @ExceptionHandler(value = { NoSuchElementException.class})
+    public ResponseEntity noSuchElement(
+            NoSuchElementException e
+    ){
+        log.error("", e);
+
+        var responsse = Api.builder()
+                .resultCode(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                .resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(responsse);
     }
 }
