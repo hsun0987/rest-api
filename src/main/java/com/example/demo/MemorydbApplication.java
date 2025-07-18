a27fd46a383d40aaf266856779596a881d20e125
hsun0987 <hsun0987@daum.net>
2025-07-18 18:06:02 +0900

    refactor Application.java

--- src/main/java/com/example/demo/MemorydbApplication.java
+++ src/main/java/com/example/demo/MemorydbApplication.java
@@ -1,11 +1,11 @@
-package com.example.exception;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExceptionApplication {
+public class MemorydbApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ExceptionApplication.class, args);
+        SpringApplication.run(MemorydbApplication.class, args);
     }
 }
--- src/main/java/com/example/validation/ValidationApplication.java
+++ src/main/java/com/example/validation/ValidationApplication.java
@@ -1,6 +1,5 @@
 package com.example.validation;
 
-import com.example.rest_api.RestApiApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
