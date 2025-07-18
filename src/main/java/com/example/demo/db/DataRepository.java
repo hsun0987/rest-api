33ba981047c01e7fa879b6e77844bfece7789cde
hsun0987 <hsun0987@daum.net>
2025-07-18 18:29:23 +0900

    [#11] Memory DataBase CRUD 01
    CRUD 추상클래스 구현체 생성

---
+++ src/main/java/com/example/demo/db/DataRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.db;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DataRepository<T, ID> extends Repository<T,ID> {
+
+    // create, update (기존에 없으면 생성, 있으면 갱신)
+    T save(T data);
+
+    // read
+    Optional<T> findById(ID id);
+    List<T> findAll();
+
+    // delete
+    void delete(ID id);
+
+
+}
